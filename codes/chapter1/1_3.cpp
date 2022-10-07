26ba66bb52c80fac7a6ec8d36226118f07ecb0e4
liuixn <963875368@qq.com>
2022-10-07 19:02:30 +0800

    cpp 学习

---
+++ codes/chapter1/1_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+
+// /**
+//  *1.嵌套注释返回的错误
+//  * warning: '/*' within block comment [-Wcomment]
+//  * 警告 注释快里面有'/*'
+//  * 
+//  * 2. std::cout<</*"*/"*/; error 注释里面包含*/
+//  */
---
+++ codes/chapter1/1_4_1.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    // unsigned int sum = 0;
+    // unsigned int index = 50;
+    // while (index<=100)
+    // {
+    //     sum+=index;
+    //     index++;
+    // }
+
+    // std::cout<<"sum:"<<sum<<std::endl;
+    
+    //2
+    // int index=10;
+    // while (index>=0)
+    // {
+    //     std::cout<<index<<std::endl;
+    //     index--;
+    // }
+
+
+    //3
+    // int max=0;
+    // int min=0;
+
+    // std::cout<<"请输入两个整数:"<<std::endl;
+
+    // std::cin>>max>>min;
+    // std::cout<<std::endl;
+    // int tmp=0;
+    // if (max<min)
+    // {
+    //    tmp=min;
+    //    min=max;
+    //    max=tmp;    
+    // }
+    // //1 3
+    // min++;//2
+    // while (min<max)
+    // {
+    //    std::cout<<min<<" ,";
+    //    min++;
+    // }
+    
+    // std::cout<<std::endl;
+    
+    
+    
+
+
+    return 0;
+}
+
+/**
+ * 1.50-100 整数相加
+ *  unsigned int sum = 0;
+    unsigned int index = 50;
+    while (index<=100)
+    {
+        sum+=index;
+        index++;
+    }
+
+    std::cout<<"sum:"<<sum<<std::endl;
+
+    2.--递减运算符答应10到0之间的整数
+    int index=10;
+    while (index>=0)
+    {
+        std::cout<<index<<std::endl;
+        index--;
+    }
+  
+  3.用户输入两个整数,打印两个整数之间的所有整数
+    int max=0;
+    int min=0;
+
+    std::cout<<"请输入两个整数:"<<std::endl;
+
+    std::cin>>max>>min;
+    std::cout<<std::endl;
+    int tmp=0;
+    if (max<min)
+    {
+       tmp=min;
+       min=max;
+       max=tmp;    
+    }
+    //1 3
+    min++;//2
+    while (min<max)
+    {
+       std::cout<<min<<" ,";
+       min++;
+    }
+    
+    std::cout<<std::endl;
+ */
