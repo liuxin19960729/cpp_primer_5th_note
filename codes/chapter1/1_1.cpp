f4499b1afffb13e0290fd1b9d8b6db35afa5dbc0
liuixn <963875368@qq.com>
2022-10-07 16:54:54 +0800

    学习

---
+++ codes/chapter1/1_1.cpp
@@ -0,0 +1,26 @@
+int main(int argc, char const *argv[])
+{
+    
+    return 2;
+}
+
+/**
+ * 
+ * g++ 1_1.cpp 
+ * 默认 可执行文件名为a.out
+ * 运行
+ * a.out
+ * 
+ * g++ 1_1.cpp  -o 1_1
+ * 编译成 名为1_1的可执行文件
+ * 运行
+ * ./1_1 
+ */
+
+/**
+ * main reture 0 
+ * echo $? // 0
+ * 
+ * main reture 2
+ * echo $? //2
+ */
