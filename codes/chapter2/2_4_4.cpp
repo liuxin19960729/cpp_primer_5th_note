c0975ecddc696156ce71d461a5948b1c8b4d1877
liuixn <963875368@qq.com>
2022-10-09 10:07:13 +0800

    cpp 学习

---
+++ codes/chapter2/2_4_4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+// typedef int &rint;
+
+
+int main(int argc, char const *argv[])
+{
+    //int null=0,*p=null;
+    // int gg=100;
+    // rint c=gg;
+    
+
+    return 0;
+}
+
+/**
+ * int null=0,*p=null;
+ * null 是一个int类型
+ * p是int 指针类型
+ * 指针类型 只能使用对应类型指针对象
+ * NULL  nullptr  0 来进行赋值
+ * 
+ */
