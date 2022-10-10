abe7cfea95eb75acc11645f5d912124a1d109bae
liuixn <963875368@qq.com>
2022-10-10 20:27:31 +0800

    cpp 学习

--- codes/chapter6/6_test.cpp
+++ codes/chapter6/6_test.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 
-void fn(const int *);
-void fn(const int c[]);
-void fn(const int c[10]);
+// void fn(const int *);
+// void fn(const int c[]);
+// void fn(const int c[10]);
+using namespace std;
+void fn(const int (&c)[2][2])
+{
+    for (auto &ar : c)
+    {
+        for (auto v : ar)
+        {
+            cout << v << endl;
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-
+    int aa[2][2] = {{1}, {2}};
+    fn(aa);
     return 0;
 }
 
-void fn(const int *) {}
-void fn(const int c[]) {}
-void fn(const int c[10]) {}
+// void fn(const int *) {}
+// void fn(const int c[]) {}
+// void fn(const int c[10]) {}
