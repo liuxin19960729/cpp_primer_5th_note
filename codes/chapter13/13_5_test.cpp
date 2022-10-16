a3d2a17745dc045184266e33367c8077def7a8dd
liuixn <963875368@qq.com>
2022-10-16 17:42:55 +0800

    cpp 学习

--- codes/chapter13/13_5_test.cpp
+++ codes/chapter13/13_5_test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <utility>
+#include <cassert>
+#include <initializer_list>
 using namespace std;
 
 class StrVec
@@ -8,6 +10,7 @@ class StrVec
 public:
     StrVec() : cap(nullptr), elements(nullptr), first_free(nullptr){};
     StrVec(const StrVec &);
+    StrVec(initializer_list<string> list);
     StrVec &operator=(const StrVec &);
     ~StrVec();
     void push_back(const string &);
@@ -15,6 +18,8 @@ public:
     size_t capacity() const { return cap - elements; }
     string *begin() const { return elements; };
     string *end() const { return first_free; }
+    int reserve(const size_t &n = 0);
+    void resize(const size_t &n);
 
 private:
     static allocator<string> alloc;
@@ -30,6 +35,7 @@ private:
             reallocate();
     }
     void reallocate();
+    int _reserve = 2;
 };
 allocator<string> StrVec::alloc;
 
@@ -39,8 +45,95 @@ void StrVec::push_back(const string &str)
     //初始化构造 并且 first_free++ 向后移动
     alloc.construct(first_free++, str);
 }
+pair<string *, string *> StrVec::alloc_n_copy(const string *b, const string *e)
+{
+    auto data = alloc.allocate(e - b);
+
+    return {data, uninitialized_copy(b, e, data)};
+}
+void StrVec::free()
+{
+    for_each(begin(), end(), [](string &p)
+             {
+          cout<<"destory()"<<endl;
+          alloc.destroy(&p); });
+
+    alloc.deallocate(elements, cap - elements);
+}
+
+StrVec::StrVec(const StrVec &strv)
+{
+    auto newData = alloc_n_copy(strv.begin(), strv.end());
+    elements = newData.first;
+    first_free = cap = newData.second;
+}
+StrVec::StrVec(initializer_list<string> list) : StrVec()
+{
+    auto newData = alloc_n_copy(list.begin(), list.end());
+    elements = newData.first;
+    first_free = cap = newData.second;
+}
+StrVec::~StrVec()
+{
+    free();
+}
+StrVec &StrVec::operator=(const StrVec &strvec)
+{
+    auto data = alloc_n_copy(strvec.begin(), strvec.end());
+    free(); //释放当前自己的内容
+    elements = data.first;
+    cap = first_free = data.second;
+    return *this;
+}
+void StrVec::reallocate()
+{
+    auto newcapacity = size() ? size() + _reserve : _reserve;
+    auto newData = alloc.allocate(newcapacity);
+    auto dest = newData;
+    auto elem = elements;
+    for (size_t i = 0; i != size(); ++i)
+    {
+        alloc.construct(dest++, move(*elem++));
+    }
+    free(); //释放旧内存
+    elements = newData;
+    first_free = dest;
+    cap = elements + newcapacity;
+}
+int StrVec::reserve(const size_t &n)
+{
+    _reserve = n > _reserve ? n : _reserve;
+    return _reserve;
+}
+void StrVec::resize(const size_t &n)
+{
+    if (n == size())
+        return;
+    const size_t sz = size();
+    for (auto i = sz; i > n; i--)
+    {
+        alloc.destroy(--first_free);
+    }
+    for (auto i = n; i < sz; i++)
+    {
+        chk_n_alloc();
+        alloc.construct(first_free++);
+    }
+}
 int main(int argc, char const *argv[])
 {
-
+    StrVec svec{"ss", "ssss"};
+    svec.push_back("liuxin");
+    for (auto &a : svec)
+    {
+        cout << a << endl;
+    }
     return 0;
 }
+/**
+ * string 拷贝
+ * 新的string修改不会影响就的string
+ * 我们对string 的拷贝实际是获取的副本
+ *
+ *
+ */
---
+++ codes/chapter13/string_cus.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <cassert>
+/**
+ *
+ *
+ */
+using namespace std;
+class string_cus
+{
+public:
+    string_cus();
+    string_cus(const char *pch);
+    string_cus(const string_cus &);
+    string_cus(string_cus &&);
+    char *begin() const { return element; }
+    char *end() const { return element ? eof : nullptr; }
+    size_t size() const { return element ? eof - element : 0; }
+    char *c_str() { return element; }
+    ~string_cus();
+
+    string_cus &operator=(const string_cus &);
+    void push_back(const char &);
+    char *insert(char *iter, const char &);
+
+private:
+    static allocator<char> alloc;
+    char *cap;
+    char *eof;
+    char *element;
+    void free();
+    void chk_alloc();
+    pair<char *, char *> realloc(const char *, const char *);
+    ssize_t step = 20;
+};
+allocator<char> string_cus::alloc;
+string_cus::string_cus() : cap(nullptr), eof(nullptr), element(nullptr) {}
+string_cus::string_cus(const char *pch)
+{
+    const char *end = pch;
+    while (*end != '\0')
+        end++;
+    auto data = realloc(pch, end);
+    element = data.first;
+    cap = eof = data.second;
+    *eof = '\0';
+}
+string_cus::string_cus(const string_cus &ostr)
+{
+    free();
+    auto data = realloc(ostr.begin(), ostr.end());
+    element = data.first;
+    cap = eof = data.second;
+    *eof = '\0';
+}
+string_cus &string_cus::operator=(const string_cus &ostr)
+{
+    free();
+    auto data = realloc(ostr.begin(), ostr.end());
+    element = data.first;
+    cap = eof = data.second;
+    *eof = '\0';
+    return *this;
+}
+string_cus::string_cus(string_cus &&str)
+{
+    free();
+    eof = str.eof;
+    element = str.element;
+    cap = str.cap;
+    str.element = nullptr;
+    cout << "right construct" << endl;
+}
+pair<char *, char *> string_cus::realloc(const char *begin, const char *end)
+{
+    const auto sz = end - begin + 1;
+    auto addr = alloc.allocate(sz);
+    return {addr, uninitialized_copy(begin, end, addr)};
+}
+void string_cus::chk_alloc()
+{
+    if (eof != cap)
+        return;
+    const auto sz = size() + step;
+    auto addr = alloc.allocate(sz);
+    auto eofaddr = uninitialized_copy(begin(), end(), addr);
+    *eof = '\0';
+    free();
+    element = addr;
+    eof = eofaddr;
+    cap = addr + sz;
+}
+string_cus::~string_cus()
+{
+    free();
+    cout << "~string_cus" << endl;
+}
+void string_cus::free()
+{
+    if (!element)
+        return;
+    alloc.deallocate(element, cap - element);
+}
+void string_cus::push_back(const char &ch)
+{
+    chk_alloc();
+    *eof++ = ch;
+    *eof = '\0';
+}
+char *string_cus::insert(char *iter, const char &ch)
+{
+    chk_alloc();
+    auto rend = eof + 1;   
+    auto lend = eof++;
+    while (iter <= lend)      
+    {
+        *rend-- = *lend--;
+    }
+    *iter = ch;
+    return ++iter;
+}
+int main(int argc, char const *argv[])
+{
+    string_cus a("ssss");
+    a.push_back('g');
+    a.insert(a.begin(), 'c');
+    a.insert(a.begin(), 'r');
+    a.insert(a.begin(), 'r');
+    cout << a.c_str() << endl;
+
+    return 0;
+}
